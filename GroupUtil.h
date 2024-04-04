10f894c97e1b1ff4d5a9c1ca1873955a739b7518
Vlad Rusu <rusu.v2k@gmail.com>
2024-04-04 04:10:32 +0300

    Add answer to BQ2 functionality - top 5 best selling products based on revenue

--- GroupUtil.h
+++ GroupUtil.h
@@ -7,8 +7,11 @@
 #include "Sales.h"
 
 void find_total_revenue_per_each_month(SalesList * sales_list);
+void find_top_5_products_by_revenue(SalesList * sales_list);
 
 void _print_compact_list_group_month_by_revenue(ListOfGroupMonthByRevenue * list);
+void _print_compact_list_group_product_by_revenue(ListOfGroupProductByRevenue * list);
 void _sort_list_of_group_month_by_revenue(ListOfGroupMonthByRevenue * list);
+void _sort_list_of_group_product_by_revenue(ListOfGroupProductByRevenue * list);
 
 #endif // GROUP_UTIL_H
